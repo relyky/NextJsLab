5b05e18333ee5e991f7f8cfdaf39cc0514eaf25b
Rely Ky <relyky@hotmail.com>
2022-05-24 00:12:07 +0800

    ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,35 @@ import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import store from '../app/store'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { zhTW } from '@mui/material/locale'
 //import Navbar from 'components/navbar/Navbar'
 import Banner from 'components/Banner'
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: '#1976d2' },
+    },
+  },
+  zhTW, // Locale text:Use the theme to configure the locale text globally.
+);
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-      <Banner />
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <Banner />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </Provider>
   )
 }
