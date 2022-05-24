7828fbad98f827598ce12227c42d2b2d36663a6f
Rely Ky <relyky@hotmail.com>
2022-05-24 12:26:34 +0800

    basic list

---
+++ src/pages/demo/dm0001/AppForm.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import InboxIcon from '@mui/icons-material/Inbox'
+import DraftsIcon from '@mui/icons-material/Drafts'
+
+//import styles from 'styles/Home.module.css'
+
+export default (props) => {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    return (
+        <Box>
+            <h1>AppForm</h1>
+            <pre>selectedIndex:{selectedIndex}</pre>
+
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton selected={selectedIndex === 1} onClick={()=>setSelectedIndex(1)}>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Item 1" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton selected={selectedIndex === 2} onClick={()=>setSelectedIndex(2)}>
+                        <ListItemIcon>
+                            <DraftsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Item 2" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton selected={selectedIndex === 3} onClick={()=>setSelectedIndex(3)}>
+                        <ListItemIcon>
+                            <DraftsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Item 3" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton selected={selectedIndex === 4} onClick={()=>setSelectedIndex(4)}>
+                        <ListItemIcon>
+                            <DraftsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Item 4" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    )
+
+}
--- src/pages/demo/dm0001/index.tsx
+++ src/pages/demo/dm0001/index.tsx
@@ -1,20 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-
-import Counter from 'components/counter/Counter'
-import styles from 'styles/Home.module.css'
+import AppForm from './AppForm'
 
 const DM0001Page: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>DM0001</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>DM0001</h1>
-      <h2>哈囉 世界好</h2>
-      <Counter />
-    </div>
+      <AppForm />
+    </>
   )
 }
 
