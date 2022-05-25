414d9e70ab3022c6f851bf57d937c3971eb7692c
Rely Ky <relyky@hotmail.com>
2022-05-25 20:51:18 +0800

    React Dnd Practice

--- src/pages/demo/dm0002/AppForm.tsx
+++ src/pages/demo/dm0002/AppForm.tsx
@@ -5,12 +5,15 @@ export default (props: object) => {
   return (
     <div>
       <h1>DEMO-0002: React DnD Lab</h1>
+      <p>參考：<a href='https://www.youtube.com/watch?v=4bzJrEETW4w&ab_channel=PedroTech' target="_blank">React Drag And Drop Tutorial - React-DND Made Simple</a></p>
       <div style={{ display: 'flex' }}>
         {/* Drag Source */}
         <Stuff title='Item A' />
         <Stuff title='Item B' />
         <Stuff title='Item C' />
       </div>
+
+      <br/>
 
       {/* Drop Target */}
       <Bucket />
