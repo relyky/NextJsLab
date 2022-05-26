2a7c43ca404b204d0da964600446e81cd364c09b
Rely Ky <relyky@hotmail.com>
2022-05-26 16:35:56 +0800

    React DnD Sortable List first commit

--- src/pages/demo/dm0003/AppForm.tsx
+++ src/pages/demo/dm0003/AppForm.tsx
@@ -1,9 +1,10 @@
+import { Container } from './Container'
 
 export default (props: object) => {
   return (
     <div>
       <h1>DEMO-0003: React DnD Sortable List Lab</h1>
-
+      <Container />
     </div>
   )
 }
