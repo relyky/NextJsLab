29dc7c38b59b89b3cba3b83c63b554fcaa69f49f
Rely Ky <relyky@hotmail.com>
2022-05-30 17:12:32 +0800

    Decision Tree UI init

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,11 @@
-import { AppBar, Toolbar, Box, Typography, Button } from '@mui/material'
+import type { ReactChild } from 'react'
+import { useState } from 'react'
+import { AppBar, Toolbar, Box, Typography, Button, Menu, MenuItem, Link } from '@mui/material'
 import AdbIcon from '@mui/icons-material/Adb'
+import DownIcon from '@mui/icons-material/ArrowDropDown'
 
 function Banner() {
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -16,24 +20,38 @@ function Banner() {
           <Button href="/main" color="inherit" sx={{ ml: 1 }}>
             Main
           </Button>
-          <Button href="/demo/dm0001" color="inherit" sx={{ ml: 1 }}>
-            Demo 001
-          </Button>
-          <Button href="/demo/dm0002" color="inherit" sx={{ ml: 1 }}>
-            Reade DnD Basic
-          </Button>
-          <Button href="/demo/dm0003" color="inherit" sx={{ ml: 1 }}>
-            Demo 003
-          </Button>
-          <Button href="/demo/dm0004" color="inherit" sx={{ ml: 1 }}>
-            Demo 004
-          </Button>
+
+          <MenuGroup label="Demo">
+            <MenuItem><Link href="/demo/dm0001" underline="none">Demo 001</Link></MenuItem>
+            <MenuItem><Link href="/demo/dm0002" underline="none">React DnD Basic</Link></MenuItem>
+            <MenuItem><Link href="/demo/dm0003" underline="none">React DnD Sortable List</Link></MenuItem>
+            <MenuItem><Link href="/demo/dm0004" underline="none">React DnD Custom Drag Layer</Link></MenuItem>
+          </MenuGroup>
+
+          <MenuGroup label="介面試作">
+            <MenuItem><Link href="/demo2/dm2010" underline="none">Decision Tree 新介面試作</Link></MenuItem>
+          </MenuGroup>
+
         </Box>
-        <Button color="inherit" sx={{ ml: 1 }}>
-          Login
-        </Button>
       </Toolbar>
     </AppBar>
+  )
+}
+
+function MenuGroup(props: { label: string, children: ReactChild | ReactChild[] }) {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const handleClick = (e) => setAnchorEl(e.currentTarget)
+  const handleClose = (_) => setAnchorEl(null)
+
+  return (
+    <>
+      <Button onClick={handleClick} color="inherit" sx={{ ml: 1 }} endIcon={<DownIcon />}>
+        {props.label}
+      </Button>
+      <Menu anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose}>
+        {props.children}
+      </Menu>
+    </>
   )
 }
 
