d334428313bb9f69d0c31ab90c9cb6dbdf9ba9d9
Rely Ky <relyky@hotmail.com>
2022-05-31 15:41:36 +0800

    decisionTree UI

--- src/pages/demo2/dm2010/AppForm.tsx
+++ src/pages/demo2/dm2010/AppForm.tsx
@@ -1,12 +1,7 @@
 import { useState } from 'react'
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Button } from '@mui/material'
+import { Container } from '@mui/material'
 import { H3, AButton } from 'components/highorder'
-import InboxIcon from '@mui/icons-material/Inbox'
-import DraftsIcon from '@mui/icons-material/Drafts'
-
 import TreeContainer from './TreeContainer'
-
-//import styles from 'styles/Home.module.css'
 
 export default (props) => {
     const [selectedIndex, setSelectedIndex] = useState(0);
@@ -16,48 +11,14 @@ export default (props) => {
     }
 
     return (
-        <Box>
+        <Container>
             <H3>DM2010: Decision Tree UI 畫面試作</H3>
             <pre>selectedIndex:{selectedIndex}</pre>
             <AButton label='測試' mutant='primary' onClick={handleClick} />
 
             <TreeContainer />
+            {/* <TreeCustomLayer /> */}
 
-            <List>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 1} onClick={() => setSelectedIndex(1)}>
-                        <ListItemIcon>
-                            <InboxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Item 1" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 2} onClick={() => setSelectedIndex(2)}>
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Item 2" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 3} onClick={() => setSelectedIndex(3)}>
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Item 3" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 4} onClick={() => setSelectedIndex(4)}>
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Item 4" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Box>
+        </Container>
     )
-
 }
--- src/pages/demo2/dm2010/StatementCard.tsx
+++ src/pages/demo2/dm2010/StatementCard.tsx
@@ -1,11 +1,14 @@
 import type { FC } from 'react'
 import type { DcsStatement } from './decisionTreeSlice'
-import { useState } from 'react'
-import { Paper, Box } from '@mui/material'
+import { useState, useMemo } from 'react'
+import { Paper, Box, Switch, Collapse, IconButton } from '@mui/material'
 import { isDcsAssignment } from './decisionTreeSlice'
 import TreeContent from './TreeContent'
 
 import WhenIcon from '@mui/icons-material/PlaylistAddCheckOutlined'
+import MoreIcon from '@mui/icons-material/MoreVert'
+import OnIcon from '@mui/icons-material/VisibilityOutlined';
+import OffIcon from '@mui/icons-material/VisibilityOffOutlined';
 
 const StatementCard: FC<{
   item: DcsStatement
@@ -13,31 +16,49 @@ const StatementCard: FC<{
   const [f_show, setShowFlag] = useState(true)
   const { isElse, cond, action } = props.item
 
-
+  const isTreeAction = !isDcsAssignment(action)
 
   return (
     <div>
       {isElse ?
         <Paper sx={{ m: 1, p: 1 }}>
-          <WhenIcon sx={{ mr: 1, verticalAlign: 'middle' }} />否則
+          <Box display="flex" alignItems="center">
+            <WhenIcon sx={{ mr: 1 }} />
+            <Box flexGrow={1}>否則</Box>
+
+            {isTreeAction && <Switch checked={f_show} onChange={_ => setShowFlag(f => !f)} />}
+
+          </Box>
         </Paper>
         :
         <Paper sx={{ m: 1, p: 1 }}>
-          <WhenIcon sx={{ mr: 1, verticalAlign: 'middle' }} />當 {cond.fdName} {codeName(cond.cmpAct)} {cond.cmpValue}, {cond.fdNote}
+          <Box display="flex" alignItems="center">
+            <WhenIcon sx={{ mr: 1 }} />
+            <Box flexGrow={1}>當 {cond.fdName} {codeName(cond.cmpAct)} {cond.cmpValue}, {cond.fdNote}</Box>
+
+            {isTreeAction && <Switch checked={f_show} onChange={_ => setShowFlag(f => !f)} />}
+
+            <IconButton color="primary">
+              <MoreIcon />
+            </IconButton>
+          </Box>
         </Paper>
       }
 
-      {isDcsAssignment(action) ?
-        <Box sx={{ m: 1, pl: '2em' }}>
-          <Paper sx={{ m: 1, p: 1 }} elevation={0}>
-            值為 {action.retValue}, {action.fdNote}
-          </Paper>
-        </Box>
-        :
-        <Box sx={{ m: 1, pl: '2em' }}>
-          <TreeContent decisionTree={action} />
-        </Box>
-      }
+      <Collapse in={f_show} >
+        {isDcsAssignment(action) ?
+          <Box sx={{ m: 1, pl: '2em' }}>
+            <Paper sx={{ m: 1, p: 1 }} elevation={0}>
+              值為 {action.retValue}, {action.fdNote}
+            </Paper>
+          </Box>
+          :
+          <Box sx={{ m: 1, pl: '2em' }}>
+            <TreeContent decisionTree={action} />
+          </Box>
+        }
+      </Collapse>
+
     </div>
   )
 }
