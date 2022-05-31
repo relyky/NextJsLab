1852f234ef1b6a73b0aec8036964f1dea013995e
Rely Ky <relyky@hotmail.com>
2022-05-31 14:10:01 +0800

    decisionTree UI

--- src/pages/demo2/dm2010/StatementCard.tsx
+++ src/pages/demo2/dm2010/StatementCard.tsx
@@ -1,31 +1,42 @@
 import type { FC } from 'react'
-import type { DcsStatement, DcsAssignment, DecisionTreeState } from './decisionTreeSlice'
+import type { DcsStatement } from './decisionTreeSlice'
+import { useState } from 'react'
+import { Paper, Box } from '@mui/material'
 import { isDcsAssignment } from './decisionTreeSlice'
 import TreeContent from './TreeContent'
+
+import WhenIcon from '@mui/icons-material/PlaylistAddCheckOutlined'
 
 const StatementCard: FC<{
   item: DcsStatement
 }> = props => {
+  const [f_show, setShowFlag] = useState(true)
   const { isElse, cond, action } = props.item
+
+
 
   return (
     <div>
       {isElse ?
-        <div>否則</div>
+        <Paper sx={{ m: 1, p: 1 }}>
+          <WhenIcon sx={{ mr: 1, verticalAlign: 'middle' }} />否則
+        </Paper>
         :
-        <div>
-          當 {cond.fdName} {codeName(cond.cmpAct)} {cond.cmpValue}, {cond.fdNote}
-        </div>
+        <Paper sx={{ m: 1, p: 1 }}>
+          <WhenIcon sx={{ mr: 1, verticalAlign: 'middle' }} />當 {cond.fdName} {codeName(cond.cmpAct)} {cond.cmpValue}, {cond.fdNote}
+        </Paper>
       }
 
       {isDcsAssignment(action) ?
-        <div style={{ marginLeft: '2em' }}>
-          值為 {action.retValue}, {action.fdNote}
-        </div>
+        <Box sx={{ m: 1, pl: '2em' }}>
+          <Paper sx={{ m: 1, p: 1 }} elevation={0}>
+            值為 {action.retValue}, {action.fdNote}
+          </Paper>
+        </Box>
         :
-        <div style={{ paddingLeft: '2em' }}>
+        <Box sx={{ m: 1, pl: '2em' }}>
           <TreeContent decisionTree={action} />
-        </div>
+        </Box>
       }
     </div>
   )
