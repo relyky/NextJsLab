4f0975ec7698ec24e4ba4024ae575dfc66b0cd58
Rely Ky <relyky@hotmail.com>
2022-05-31 09:50:41 +0800

    highorder component

--- src/components/highorder.tsx
+++ src/components/highorder.tsx
@@ -1,8 +1,21 @@
-import type { FC, ReactChild } from 'react'
-import { Typography } from "@mui/material"
+import type { FC, MouseEventHandler, ReactChild } from 'react'
+import { Typography, Button } from "@mui/material"
 
-export const H3: FC<{ text?: string, children?: ReactChild }> = (props) => (
+export const H3: FC<{
+    text?: string,
+    children?: ReactChild
+}> = props => (
     <Typography variant='h3'>
         {props.text ? props.text : props.children}
     </Typography>
+)
+
+export const AButton: FC<{
+    mutant: 'primary' | 'secondary',
+    label: string,
+    onClick: MouseEventHandler<HTMLButtonElement>
+}> = props => (
+    <Button color={props.mutant} variant='contained' onClick={props.onClick} >
+        {props.label}
+    </Button>
 )
