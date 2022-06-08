1be6464e5545078d2d77a6650309d7f051c3ab14
Rely Ky <relyky@hotmail.com>
2022-06-08 23:44:11 +0800

    hover lab

--- src/pages/demo/dm0001/AppForm.tsx
+++ src/pages/demo/dm0001/AppForm.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton, Divider } from '@mui/material'
 import InboxIcon from '@mui/icons-material/Inbox'
 import DraftsIcon from '@mui/icons-material/Drafts'
+import DeleteIcon from '@mui/icons-material/Delete';
+import ss from './AppForm.module.css'
 
 //import styles from 'styles/Home.module.css'
 
@@ -13,33 +15,52 @@ export default (props) => {
             <h1>AppForm</h1>
             <pre>selectedIndex:{selectedIndex}</pre>
 
+            <p>測試 item.hover </p>
+
+
             <List>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 1} onClick={()=>setSelectedIndex(1)}>
+                <ListItem disablePadding secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 1} onClick={() => setSelectedIndex(1)}>
                         <ListItemIcon>
                             <InboxIcon />
                         </ListItemIcon>
                         <ListItemText primary="Item 1" />
                     </ListItemButton>
                 </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 2} onClick={()=>setSelectedIndex(2)}>
+                <ListItem disablePadding secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 2} onClick={() => setSelectedIndex(2)}>
                         <ListItemIcon>
                             <DraftsIcon />
                         </ListItemIcon>
                         <ListItemText primary="Item 2" />
                     </ListItemButton>
                 </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 3} onClick={()=>setSelectedIndex(3)}>
+                <ListItem disablePadding secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 3} onClick={() => setSelectedIndex(3)}>
                         <ListItemIcon>
                             <DraftsIcon />
                         </ListItemIcon>
                         <ListItemText primary="Item 3" />
                     </ListItemButton>
                 </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton selected={selectedIndex === 4} onClick={()=>setSelectedIndex(4)}>
+                <ListItem disablePadding secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 4} onClick={() => setSelectedIndex(4)}>
                         <ListItemIcon>
                             <DraftsIcon />
                         </ListItemIcon>
@@ -47,6 +68,56 @@ export default (props) => {
                     </ListItemButton>
                 </ListItem>
             </List>
+
+            <Divider sx={{ my: 3 }} />
+
+            <List>
+                <ListItem className={ss.item} disablePadding secondaryAction={
+                    <IconButton className={ss.command} edge="end" aria-label="delete">
+                        <DraftsIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 1} onClick={() => setSelectedIndex(1)}>
+                        <ListItemText primary="Item 1" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem className={ss.item} disablePadding secondaryAction={
+                    <IconButton className={ss.command} edge="end" aria-label="delete">
+                        <InboxIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 2} onClick={() => setSelectedIndex(2)}>
+                        <ListItemText primary="Item 2" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem className={ss.item} disablePadding secondaryAction={
+                    <IconButton className={ss.command} edge="end" aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                }>
+                    <ListItemButton selected={selectedIndex === 3} onClick={() => setSelectedIndex(3)}>
+                        <ListItemText primary="Item 3" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem className={ss.item} disablePadding secondaryAction={
+                    <div className={ss.command}>
+                        <IconButton edge="end" sx={{ ml: .5 }}>
+                            <DraftsIcon />
+                        </IconButton>
+                        <IconButton edge="end" sx={{ ml: .5 }}>
+                            <InboxIcon />
+                        </IconButton>
+                        <IconButton edge="end" sx={{ ml: .5 }}>
+                            <DeleteIcon />
+                        </IconButton>
+                    </div>
+                }>
+                    <ListItemButton selected={selectedIndex === 4} onClick={() => setSelectedIndex(4)}>
+                        <ListItemText primary="Item 4" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+
         </Box>
     )
 
