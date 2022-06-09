09975bd9ae83876bf8bfcea462013cb89e3dee1b
Rely Ky <relyky@hotmail.com>
2022-06-09 14:27:34 +0800

    TreeView Lab

---
+++ src/pages/demo/dm0005/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import AppForm from './AppForm'
+
+const DM0005Page: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>DM0005</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AppForm />
+    </>
+  )
+}
+
+export default DM0005Page
