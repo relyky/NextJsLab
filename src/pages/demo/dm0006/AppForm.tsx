c5f0afb24e7cf75ce0389289fb184a972eb221ac
Rely Ky <relyky@hotmail.com>
2022-06-15 16:25:23 +0800

    diff2 參數化

--- src/pages/demo/dm0006/AppForm.tsx
+++ src/pages/demo/dm0006/AppForm.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react'
 import { useState, useEffect, useMemo } from 'react'
 import { useAppSelector } from 'hooks/hooks'
-import { Container, Paper, Stack, Box, Collapse } from '@mui/material'
-import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel, FormGroup, Checkbox, InputLabel, Input } from '@mui/material'
+import { Container, Paper, Stack, Box, Collapse, Grid } from '@mui/material'
+import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel, FormGroup, Checkbox, TextField } from '@mui/material'
 import { H3, H4, H5, H6, P1, P2, AButton, ASwitch } from 'components/highorder'
 import Swal from 'sweetalert2'
 
@@ -51,23 +51,23 @@ export default function AppForm(props) {
     const [f_originalDiff, setOriginalDiff] = useState(false)
 
     // diff options 參數
+    const [patchOption, setPatchOption] = useState<PatchOptions>({
+        context: 100, // 顯示關聯行數 default:4
+        ignoreWhitespace: false, //
+        newlineIsToken: false,
+    })
 
     // diff2html 參數
     const [htmlOption, setHtmlOption] = useState<Diff2HtmlConfig>({
-        matching: "none", // 'lines' | 'words' | 'none', default:'none'
+        matching: "lines", // 'lines' | 'words' | 'none', default:'none'
         drawFileList: false,
-        outputFormat: "line-by-line" // 'line-by-line' | 'side-by-side', default:'line-by-line'
+        outputFormat: "side-by-side" // 'line-by-line' | 'side-by-side', default:'line-by-line'
     })
 
     // generate unified diff patch
-    const options: PatchOptions = {
-        context: 2500, // 顯示關聯行數 default:4
-        ignoreWhitespace: false, //
-        newlineIsToken: false,
-    }
+    const unifiedDiffPatch = createTwoFilesPatch(filename, filename, oldStr, newStr, oldVersion, newVersion, patchOption);
 
-    const unifiedDiffPatch = createTwoFilesPatch(filename, filename, oldStr, newStr, oldVersion, newVersion, options);
-
+    // convert patch to html
     const outputHtml = useMemo(() => html(unifiedDiffPatch, htmlOption),
         [unifiedDiffPatch, htmlOption]
     );
@@ -94,8 +94,10 @@ export default function AppForm(props) {
                 <ASwitch label='顯示原始 unified diff patch' value={f_originalDiff} onChange={v => setOriginalDiff(v.value)} />
                 <br />
 
+                <DiffPatchOptionPanel option={patchOption} onChange={setPatchOption} />
+
                 <HtmlOpitonPanel option={htmlOption} onChange={setHtmlOption} />
-                
+
             </Box>
 
             <Collapse in={f_originalDiff}>
@@ -112,50 +114,88 @@ export default function AppForm(props) {
     )
 }
 
-//-------------------------------
-//  [htmlOption, setHtmlOption] = useState<Diff2HtmlConfig>
+//-----------------------------------------------------------------------------
 const HtmlOpitonPanel: FC<{
     option: Diff2HtmlConfig,
     onChange: (option: Diff2HtmlConfig) => void
 }> = props => {
     const { option } = props
-    //function HtmlOpitonPanel(option: Diff2HtmlConfig, onChange: (option: Diff2HtmlConfig) => void) {
     return (
-        <Box>
-            <FormControl variant='filled'>
-                <FormLabel>outputFormat</FormLabel>
-                <RadioGroup row
-                    name="outputFormat"
-                    value={option.outputFormat}
-                    onChange={(e, v: OutputFormatType) => props.onChange({ ...option, outputFormat: v })}
-                >
-                    <FormControlLabel value="line-by-line" control={<Radio />} label="line-by-line" />
-                    <FormControlLabel value="side-by-side" control={<Radio />} label="side-by-side" />
-                </RadioGroup>
-            </FormControl>
+        <div>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <FormControl variant='filled'>
+                        <FormLabel>outputFormat</FormLabel>
+                        <RadioGroup row
+                            name="outputFormat"
+                            value={option.outputFormat}
+                            onChange={(e, v: OutputFormatType) => props.onChange({ ...option, outputFormat: v })}
+                        >
+                            <FormControlLabel value="line-by-line" control={<Radio />} label="line-by-line" />
+                            <FormControlLabel value="side-by-side" control={<Radio />} label="side-by-side" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={3}>
+                    <FormControl>
+                        <FormLabel>matching</FormLabel>
+                        <RadioGroup row
+                            name="matching"
+                            value={option.matching}
+                            onChange={(e, v: LineMatchingType) => props.onChange({ ...option, matching: v })}
+                        >
+                            <FormControlLabel value="none" control={<Radio />} label="none" />
+                            <FormControlLabel value="lines" control={<Radio />} label="lines" />
+                            <FormControlLabel value="words" control={<Radio />} label="words" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={3}>
+                    <FormControl>
+                        <FormLabel>drawFileList</FormLabel>
+                        <FormGroup>
+                            <FormControlLabel label="drawFileList" control={
+                                <Checkbox value={option.drawFileList} onChange={(e, v) => props.onChange({ ...option, drawFileList: v })} />
+                            } />
+                        </FormGroup>
+                    </FormControl>
+                </Grid>
+            </Grid>
 
-            <FormControl>
-                <FormLabel>matching</FormLabel>
-                <RadioGroup row
-                    name="matching"
-                    value={option.matching}
-                    onChange={(e, v: LineMatchingType) => props.onChange({ ...option, matching: v })}
-                >
-                    <FormControlLabel value="none" control={<Radio />} label="none" />
-                    <FormControlLabel value="lines" control={<Radio />} label="lines" />
-                    <FormControlLabel value="words" control={<Radio />} label="words" />
-                </RadioGroup>
-            </FormControl>
+        </div>
+    )
+}
 
-            <FormControl>
-                <FormLabel>drawFileList</FormLabel>
-                <FormGroup>
-                    <FormControlLabel label="drawFileList" control={
-                        <Checkbox value={option.drawFileList} onChange={(e, v) => props.onChange({ ...option, drawFileList: v })} />
+//-----------------------------------------------------------------------------
+const DiffPatchOptionPanel: FC<{
+    option: PatchOptions,
+    onChange: (option: PatchOptions) => void
+}> = props => {
+    const { option } = props
+    return (
+        <div>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <TextField
+                        label="context"
+                        type="number"
+                        variant="standard"
+                        value={option.context}
+                        onChange={e => props.onChange({ ...option, context: Number(e.target.value) })}
+                        helperText="顯示差異前後關聯行數。"
+                    />
+                </Grid>
+                <Grid item xs={3}>
+                    <FormControlLabel label="ignoreWhitespace" control={
+                        <Checkbox value={option.ignoreWhitespace} onChange={(e, v) => props.onChange({ ...option, ignoreWhitespace: v })} />
                     } />
-                </FormGroup>
-            </FormControl>
-        </Box>
-
+                </Grid>
+                <Grid item xs={3}>
+                    <FormControlLabel label="newlineIsToken" control={
+                        <Checkbox value={option.newlineIsToken} onChange={(e, v) => props.onChange({ ...option, newlineIsToken: v })} />
+                    } />
+                </Grid>
+            </Grid>
+        </div>
     )
 }
