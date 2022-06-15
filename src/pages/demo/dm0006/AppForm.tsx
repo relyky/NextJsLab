1edef14171f22c148da3f1f71764051ff3ee23d3
Rely Ky <relyky@hotmail.com>
2022-06-15 12:04:54 +0800

    jsdiff lab commit

--- src/pages/demo/dm0006/AppForm.tsx
+++ src/pages/demo/dm0006/AppForm.tsx
@@ -31,15 +31,15 @@ const oldStr = `開始
 `
 const newVersion = 'version 2'
 const newStr = `開始
-當 Staff = Y, 是否為DBS員工
-　　值為 A,
-當 Small_White = X, 信用大白(有JCIC紀錄申請)
+當 Small_White = X, 信用小白(有JCIC紀錄申請)
 　　值為 C,
 當 職稱 in '16','29','36','34', Customer Segment
 　　當 客戶層級 = VIP, Customer Segment
 　　　值為 Z,
 　　否則
 　　　　值為 GUEST, 來賓層級
+當 Big_Black = PQ, 信用大黑(士紳土豪)
+　　值為 V, 賺翻了
 否則
 　　值為 Otherwise, 其他
 `
@@ -55,7 +55,7 @@ export default function AppForm(props) {
     const outputHtml = html(unifiedDiffPatch, {
         matching: "lines", // 'lines' | 'words' | 'none', default:'none'
         drawFileList: false,
-        outputFormat: "line-by-line" // 'line-by-line' | 'side-by-side', default:'line-by-line'
+        outputFormat: "side-by-side" // 'line-by-line' | 'side-by-side', default:'line-by-line'
     });
 
     return (
@@ -80,7 +80,7 @@ export default function AppForm(props) {
                 <pre style={{ textAlign: "left" }}>{unifiedDiffPatch}</pre>
             </Paper>
 
-            <H4>unified diff patch 2 HTML</H4>
+            <H4>轉換 unified diff patch 成 HTML</H4>
             {htmlRenderer(outputHtml)}
 
         </Container>
