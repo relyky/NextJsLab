fae0f077e78e048427b6f5d99b8f8e418eb218b1
Rely Ky <relyky@hotmail.com>
2022-06-16 22:32:44 +0800

    reactSpring lab commit

--- src/pages/demo/dm0007/AppForm.tsx
+++ src/pages/demo/dm0007/AppForm.tsx
@@ -1,16 +1,9 @@
-import type { FC } from 'react'
-import { useEffect, useState } from 'react'
-import { Container, Box, Paper, Divider } from '@mui/material'
-import { H3, P1, ASwitch } from 'components/highorder'
-// icons
-import InboxIcon from '@mui/icons-material/Inbox'
-import DraftsIcon from '@mui/icons-material/Drafts'
-import DeleteIcon from '@mui/icons-material/Delete';
+import type { CSSProperties, FC } from 'react'
+import { useState } from 'react'
+import { Container, Paper, Divider } from '@mui/material'
+import { H3, P1, AButton } from 'components/highorder'
 // css
 import { useSpring, animated, config } from 'react-spring'
-import ss from './AppForm.module.css'
-
-//import styles from 'styles/Home.module.css'
 
 export default function AppForm() {
     const [f_show, setShow] = useState(true)
@@ -20,8 +13,7 @@ export default function AppForm() {
         <Container>
             <H3>DM0007: react-spring Lab</H3>
 
-            <P1>第一個練習：進場動畫 fade in。</P1>
-
+            <P1>第一個練習: 進場動畫 fade in。</P1>
             <SpringBasic1_FadeIn>
                 <Paper sx={{ p: 3 }}>
                     <H3 >I will fade in</H3>
@@ -29,18 +21,20 @@ export default function AppForm() {
             </SpringBasic1_FadeIn>
 
             <Divider sx={{ m: 1 }} />
-            <P1>第二個練習：翻動 filp text。</P1>
-
+            <P1>第二個練習: 翻動 filp text。</P1>
             <SpringBasic2_FlipText>
                 <H3>hello world</H3>
             </SpringBasic2_FlipText>
 
-            {/* <ASwitch label="show animated" value={f_show} onChange={v => setShow(v.value)} /> */}
-
             <Divider sx={{ m: 1 }} />
             <P1>第三個練習: 透視變化過程: 0→1→0→1→...。</P1>
-
             <SpringBasic3_Number />
+
+            <Divider sx={{ m: 1 }} />
+            <P1>第四個練習: api 用程式觸發CSS變化。</P1>
+            <SpringBasic4_SpringApi>
+                <H3>hello world</H3>
+            </SpringBasic4_SpringApi>
 
         </Container>
     )
@@ -70,9 +64,17 @@ const SpringBasic2_FlipText: FC = (props) => {
     })
 
     return (
-        <animated.div style={aniStyle}>
-            {props.children}
-        </animated.div>
+        <div>
+            {/* animated element 方式 */}
+            <animated.div style={aniStyle}>
+                {props.children}
+            </animated.div>
+
+            {/* React components 結合 srping 方式 */}
+            <AniH3 style={aniStyle}>
+                {props.children}
+            </AniH3>
+        </div>
     )
 }
 
@@ -96,3 +98,50 @@ const SpringBasic3_Number: FC = (props) => {
         </animated.div>
     )
 }
+
+//-----------------------------------------------------------------------------
+const SpringBasic4_SpringApi: FC = (props) => {
+    const [aniStyle, api] = useSpring(() => ({ opacity: 1 }))
+    // CSS 初始狀態:顯現
+
+    return (
+        <div>
+            <AButton mutant='primary' label="隱藏 "
+                onClick={() => api.start({ opacity: 0 })} // CSS 變化目標:隱藏
+            />
+            <AButton mutant='primary' label="顯現"
+                onClick={() => api.start({ opacity: 1 })} // CSS 變化目標:顯現
+            />
+
+            {/* animated element 方式 */}
+            <animated.div style={aniStyle}>
+                {props.children}
+            </animated.div>
+
+            {/* React components 結合 srping 方式 */}
+            <AniH3 style={aniStyle}>
+                {props.children}
+            </AniH3>
+        </div>
+    )
+}
+
+//-----------------------------------------------------------------------------
+//## 一個簡單的 React 元件
+// 將用來 結合 spring.animated。
+// 其中 props 至少要有 style 屬性。
+const MyH3: FC<{
+    style: CSSProperties
+}> = props => {
+    const baseStyle = { color: 'red' }
+    return (
+        <h3 style={{ ...baseStyle, ...props.style }}>
+            {props.children}
+        </h3>
+    )
+}
+
+//## 將 React 元件結合 spring.animated
+// 把 spring style 物件轉換成 CSSProperties
+const AniH3 = animated(MyH3)
+ 
