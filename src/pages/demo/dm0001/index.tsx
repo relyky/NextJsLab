adf7a0002240021a39359ccce0b93b25b10bcecc
Rely Ky <relyky@hotmail.com>
2022-07-07 01:18:28 +0800

    修正 routing

--- src/pages/demo/dm0001/index.tsx
+++ src/pages/demo/dm0001/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import AppForm from './AppForm'
+import AppForm from 'views/demo/dm0001/AppForm'
 
 const DM0001Page: NextPage = () => {
   return (
--- src/pages/demo/dm0002/index.tsx
+++ src/pages/demo/dm0002/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { DndProvider } from 'react-dnd'
-import AppForm from './AppForm'
+import AppForm from 'views/demo/dm0002/AppForm'
 
 const DM0002Page: NextPage = () => {
   return (
