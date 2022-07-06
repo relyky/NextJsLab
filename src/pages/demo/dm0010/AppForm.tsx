3b4c1d534692887bccb37c73eee142ffc83c45a1
Rely Ky <relyky@hotmail.com>
2022-07-06 17:31:02 +0800

    SSR commit

--- src/pages/demo/dm0010/AppForm.tsx
+++ src/pages/demo/dm0010/AppForm.tsx
@@ -1,16 +1,26 @@
-import { useEffect, useState } from 'react'
 import type { Commodity } from './interfaces'
+import { useState } from 'react'
 import { qryCommodityList } from './bizLogic'
 
 export default (props: {
-    commodityList: Commodity[]
+    commodityList: Commodity[] // 初始商品目錄將從 props 送進來。
 }) => {
+    // 初始商品目錄來自SSR模式。
+    const [commodityList, setCommodityList] = useState<Commodity[]>(props.commodityList)
+
+    // 之後操作查詢商品目錄來自CSR模式。
+    function handleQuery() {
+        const commodityList = qryCommodityList('other')
+        setCommodityList(commodityList)
+    }
+
     return (
         <div>
             <h1>DM0010: SSR 練習</h1>
+            <button onClick={handleQuery}>查詢</button>
             <h2>模擬商品清單</h2>
             <ul>
-                {Array.isArray(props.commodityList) && props.commodityList.map((item, index) => (
+                {Array.isArray(commodityList) && commodityList.map((item, index) => (
                     <li key={index}>
                         {`${item.cid}-${item.cname} 數量 ${item.amount} | ${item.category}`}
                     </li>
