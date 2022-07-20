0834b18bb658df6a5ed423fd092cee9404b2f6d1
Rely Ky <relyky@hotmail.com>
2022-07-20 23:27:25 +0800

    deploy next.js on IIS web server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 8080
+const port = process.env.PORT || 3000
+
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -33,6 +33,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
