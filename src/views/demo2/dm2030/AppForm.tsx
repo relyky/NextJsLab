bdc5e37b3ee3d579ad4b17f1f3c51b65a26920d0
Rely Ky <relyky@hotmail.com>
2022-08-05 14:26:50 +0800

    TransitionGroup sample

--- src/views/demo2/dm2030/AppForm.tsx
+++ src/views/demo2/dm2030/AppForm.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { Container, Box, Divider, Paper, IconButton, Avatar } from '@mui/material'
-import { List, ListItem, ListItemAvatar, ListItemText, ListItemIcon } from '@mui/material'
-import { FormControl, OutlinedInput } from '@mui/material'
-import { H3 } from 'components/highorder'
-import { useAppSelector, useAppDispatch } from 'hooks/hooks'
-import ss from './AppForm.module.css'
+import { Container, Box, Divider, Paper, IconButton } from '@mui/material'
+import { List, ListItem, ListItemText, ListItemIcon } from '@mui/material'
+import { OutlinedInput } from '@mui/material'
+import { H3, P1 } from 'components/highorder'
 // hooks
-import { addItem, rmvItem, shiftOutItem, toggleItem } from 'views/demo2/dm2030/todoListSlice'
+import { useState } from 'react'
+import { useAppSelector, useAppDispatch } from 'hooks/hooks'
+import { addItem, rmvItem, toggleItem } from 'views/demo2/dm2030/todoListSlice'
 // icons
 import DoneIcon from '@mui/icons-material/CheckCircle';
 import UndoIcon from '@mui/icons-material/RadioButtonUnchecked';
 import ClearIcon from '@mui/icons-material/Clear'
 // CSS
 import clsx from 'clsx'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default (props) => {
     const [newText, setNewText] = useState('');
@@ -22,39 +22,50 @@ export default (props) => {
     return (
         <Container>
             <H3>Todos</H3>
+            <P1>新增(mount)與移除(unmount)有加入過場動畫。</P1>
 
             <Paper sx={{ p: 2 }}>
-                <FormControl fullWidth>
-                    <OutlinedInput placeholder="What needs to bo done?"
-                        value={newText}
-                        onChange={e => setNewText(e.target.value)}
-                        onKeyUp={e => {
-                            if (e.key === 'Enter') {
-                                dispatch(addItem(newText))
-                                setNewText('')
-                            }
-                        }}
-                    />
-                </FormControl>
+                <OutlinedInput placeholder="What needs to bo done?"
+                    fullWidth
+                    value={newText}
+                    onChange={e => setNewText(e.target.value)}
+                    onKeyUp={e => {
+                        if (e.key === 'Enter') {
+                            dispatch(addItem(newText))
+                            setNewText('')
+                        }
+                    }}
+                />
                 <List>
-                    {todoList.map((todo) => (
-                        <ListItem key={todo.id} sx={{ p: 2, mb: 1 }}
-                            onAnimationEnd={e => (e.animationName === 'fadeOutDown') && dispatch(rmvItem(todo.id))}
-                            className={clsx("animate__animated", { "animate__fadeInUp": !todo.outer, "animate__fadeOutDown": todo.outer })}
-                            secondaryAction={
-                                <IconButton edge="end" onClick={() => dispatch(shiftOutItem(todo.id))}>
-                                    <ClearIcon />
-                                </IconButton>
-                            }
-                        >
-                            <ListItemIcon onClick={() => dispatch(toggleItem(todo.id))} >
-                                {todo.completed ? <DoneIcon color="success" /> : <UndoIcon />}
-                            </ListItemIcon>
-                            <ListItemText>
-                                {todo.text}
-                            </ListItemText>
-                        </ListItem>
-                    ))}
+                    <TransitionGroup>
+                        {todoList.map((todo) => (
+                            <CSSTransition
+                                key={todo.id}
+                                timeout={400}
+                                classNames={{
+                                    enter: "animate__animated",
+                                    enterActive: "animate__fadeInUp",
+                                    exit: "animate__animated",
+                                    exitActive: "animate__backOutDown"
+                                }}
+                            >
+                                <ListItem sx={{ p: 2, mb: 1 }}
+                                    secondaryAction={
+                                        <IconButton edge="end" onClick={() => dispatch(rmvItem(todo.id))}>
+                                            <ClearIcon />
+                                        </IconButton>
+                                    }
+                                >
+                                    <ListItemIcon onClick={() => dispatch(toggleItem(todo.id))} >
+                                        {todo.completed ? <DoneIcon color="success" /> : <UndoIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {todo.text}
+                                    </ListItemText>
+                                </ListItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
                 </List>
             </Paper>
         </Container >
--- src/views/demo2/dm2030/todoListSlice.ts
+++ src/views/demo2/dm2030/todoListSlice.ts
@@ -1,20 +1,17 @@
-import type { SecUnit } from 'pages/api/form09/interfaces'
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { AppState, AppThunk } from 'store/store'
-import { Color } from '@mui/material'
 
 export interface ToDoItem {
   id: number, // a unique number
   text: string, // the text the user typed in
   completed: boolean, // a boolean flag
   color: string, // An optional color category
-  outer: boolean,
 }
 
 const initialState: ToDoItem[] = [
-  { id: 1, text: 'Learn React', completed: true, color: null, outer: false },
-  { id: 2, text: 'Learn Redux', completed: false, color: 'purple', outer: false },
-  { id: 3, text: 'Build something fun!', completed: false, color: 'blue', outer: false },
+  { id: 1, text: 'Learn React', completed: true, color: null },
+  { id: 2, text: 'Learn Redux', completed: false, color: 'purple' },
+  { id: 3, text: 'Build something fun!', completed: false, color: 'blue' },
 ]
 
 export const todoListSlice = createSlice({
@@ -28,8 +25,7 @@ export const todoListSlice = createSlice({
         id: newId,
         text: action.payload,
         completed: false,
-        color: null,
-        outer: false
+        color: null
       };      
       todoList.push(newItem)
     },
@@ -37,13 +33,6 @@ export const todoListSlice = createSlice({
       const itemId = action.payload
       const itemIdx = todoList.findIndex(c => c.id === itemId)
       todoList.splice(itemIdx, 1)
-    },
-    shiftOutItem: (todoList /* state */, action: PayloadAction<number>) => {
-      const itemId = action.payload
-      todoList.map(cur => {
-        if(cur.id === itemId) cur.outer = true;
-        return cur
-      })
     },
     toggleItem: (todoList /* state */, action: PayloadAction<number>) => {
       const itemId = action.payload
@@ -59,7 +48,6 @@ export const {
   addItem,
   rmvItem,
   toggleItem,
-  shiftOutItem,
 } = todoListSlice.actions
 
 export default todoListSlice.reducer
